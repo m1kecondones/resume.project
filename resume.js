b584b886b43b1aff3b21d18e134b53715d29c977
Michael Tagarira <othertino03@gmail.com>
2025-09-17 08:19:59 -0600

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,15 @@
+//Define Variables
+let myName = "Michael Tagarira";
+let myEmail = "othertino03@oru.edu";
+let title = "Student";
+let education = "Oral Roberts University";
+let experience = "Summer camp worker, Maintenance, Kids and Teens Camp counselor";
+let skills = ["Basketball", "Manual labour", "Extroverted"];
+//let skills = "something"; //TODO
+
+//Connect Variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("my-email").textContent = myEmail;
+document.getElementById("title").textContent = title;
+document.getElementById("education").textContent = education ;
+document.getElementById("experience").textContent = experience ;
